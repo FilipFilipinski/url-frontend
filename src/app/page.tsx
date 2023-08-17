4a647729ed589c495be4926d48ceb4c33c93f67f
Max Cyprys <mc.cyprysik@icloud.com>
2023-08-17 12:41:19 +0200

    fix: remove app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label"
 
 export default function Home() {
   return (
-    <div className='container flex justify-center mt-12'>
+    <div className='container flex justify-center mt-20'>
       <Card className='lg:w-1/2'>
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl">Login</CardTitle>
